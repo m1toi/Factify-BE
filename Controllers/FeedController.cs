fed5d7be6842447515fb91517aa54d2586b37eb4
m1toi <147705211+m1toi@users.noreply.github.com>
2025-03-15 16:52:26 +0200

    implement feed controller

---
+++ Controllers/FeedController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SocialMediaApp.BusinessLogic.Services.FeedService;
+
+namespace SocialMediaApp.Controllers
+{
+	[Route("api/Feed")]
+	public class FeedController : ControllerBase
+	{
+		private readonly IFeedService _feedService;
+
+		public FeedController(IFeedService feedService)
+		{
+			_feedService = feedService;
+		}
+
+		[HttpGet("{userId}")]
+		[ProducesResponseType(StatusCodes.Status200OK)]
+		[ProducesResponseType(StatusCodes.Status404NotFound)]
+		public ActionResult GetPersonalizedFeed([FromRoute] int userId)
+		{
+			return Ok(_feedService.GetPersonalizedFeed(userId));
+		}
+	}
+}
